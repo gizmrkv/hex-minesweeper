f4f4b783a90e8bc573e07e237f441e8cb3eba047
gizmrkv <gizmrkv@gmail.com>
2023-01-29 05:29:45 +0900

    add: events pluginを追加

--- src/events.rs
+++ src/events.rs
@@ -1,4 +1,5 @@
 use crate::hexgrid::PointyHexGrid;
+use bevy::prelude::*;
 
 pub struct OnTryOpenTile {
     target: PointyHexGrid,
@@ -13,7 +14,23 @@ pub enum OnMoveTile {
     Flag { target: PointyHexGrid },
 }
 
+pub struct OnUndoMoveTile;
+
 pub struct OnGameOver;
 pub struct OnGameClear;
 
 pub struct OnQuitGame;
+
+pub struct EventsPlugin;
+
+impl Plugin for EventsPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_event::<OnTryOpenTile>()
+            .add_event::<OnTryFlagTile>()
+            .add_event::<OnMoveTile>()
+            .add_event::<OnUndoMoveTile>()
+            .add_event::<OnGameOver>()
+            .add_event::<OnGameClear>()
+            .add_event::<OnQuitGame>();
+    }
+}
