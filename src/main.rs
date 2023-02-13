4330fc739f970b679e2110fbf13f6beac22cf221
gizmrkv <gizmrkv@gmail.com>
2023-02-13 22:24:45 +0900

    add: GameStateを追加

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@ use bevy::prelude::*;
 
 mod hexagonal_coordinate;
 
+#[derive(Clone, PartialEq, Eq, Debug, Hash)]
+enum GameState {
+    Title,
+}
+
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_state(GameState::Title)
+        .run();
 }
