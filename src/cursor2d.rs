b90fdcd85d601ee84733bac5d587aedc0070b352
gizmrkv <gizmrkv@gmail.com>
2023-02-13 22:50:51 +0900

    add: Cursor2dを追加

---
+++ src/cursor2d.rs
@@ -0,0 +1,43 @@
+use bevy::render::camera::RenderTarget;
+use bevy::{prelude::*, ui::update};
+
+pub struct Cursor2dPlugin;
+
+impl Plugin for Cursor2dPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(Cursor2d::default())
+            .add_system(update_cursor);
+    }
+}
+
+#[derive(Resource, Default)]
+pub struct Cursor2d {
+    pub screen_position: Vec2,
+    pub world_position: Vec2,
+}
+
+fn update_cursor(
+    windows: Res<Windows>,
+    camera_query: Query<(&Camera, &GlobalTransform)>,
+    mut cursor2d: ResMut<Cursor2d>,
+) {
+    let (camera, camera_transform) = camera_query.single();
+    let wnd = if let RenderTarget::Window(id) = camera.target {
+        windows.get(id).unwrap()
+    } else {
+        windows.get_primary().unwrap()
+    };
+
+    if let Some(screen_position) = wnd.cursor_position() {
+        let window_size = Vec2::new(wnd.width() as f32, wnd.height() as f32);
+        // convert screen position [0..resolution] to ndc [-1..1] (gpu coordinates)
+        let ndc = (screen_position / window_size) * 2.0 - Vec2::ONE;
+        // matrix for undoing the projection and camera transform
+        let ndc_to_world = camera_transform.compute_matrix() * camera.projection_matrix().inverse();
+        // use it to convert ndc to world-space coordinates
+        let world_position = ndc_to_world.project_point3(ndc.extend(-1.0));
+
+        cursor2d.screen_position = screen_position;
+        cursor2d.world_position = world_position.truncate();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use bevy::log::LogPlugin;
 use bevy::prelude::*;
 
+mod cursor2d;
 mod hexagonal_coordinate;
 
 #[derive(Clone, PartialEq, Eq, Debug, Hash)]
@@ -9,7 +11,16 @@ enum GameState {
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(LogPlugin {
+            filter: "info,wgpu_core=warn,wgpu_hal=warn,mygame=debug".into(),
+            level: bevy::log::Level::DEBUG,
+        }))
+        .add_plugin(cursor2d::Cursor2dPlugin)
         .add_state(GameState::Title)
+        .add_startup_system(spawn_camera)
         .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
