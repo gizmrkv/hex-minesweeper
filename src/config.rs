d95d70f14a8e1168d28e9c732eef9270ce42550e
gizmrkv <gizmrkv@gmail.com>
2023-02-14 02:00:20 +0900

    add: configを追加

---
+++ src/config.rs
@@ -0,0 +1,53 @@
+use bevy::{
+    asset::{AssetLoader, LoadContext, LoadedAsset},
+    prelude::*,
+    reflect::TypeUuid,
+    utils::BoxedFuture,
+};
+use serde::Deserialize;
+
+/// Plugin to allow access to app config.
+pub struct ConfigPlugin;
+
+impl Plugin for ConfigPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_asset::<Config>()
+            .init_asset_loader::<ConfigLoader>()
+            .add_startup_system(load_config);
+    }
+}
+
+/// Load a config and spawn its handle.
+fn load_config(asset_server: Res<AssetServer>, mut commands: Commands) {
+    let handle: Handle<Config> = asset_server.load("config/config.ron");
+    commands.spawn(handle);
+}
+
+/// app config.
+#[derive(Debug, Deserialize, TypeUuid, Default, Clone, Copy)]
+#[uuid = "39cadc56-aa9c-4543-8640-a018b74b5052"]
+pub struct Config {
+    pub background_color: Color,
+}
+
+/// app config loader.
+#[derive(Default)]
+struct ConfigLoader;
+
+impl AssetLoader for ConfigLoader {
+    fn load<'a>(
+        &'a self,
+        bytes: &'a [u8],
+        load_context: &'a mut LoadContext,
+    ) -> BoxedFuture<'a, Result<(), bevy::asset::Error>> {
+        Box::pin(async move {
+            let config = ron::de::from_bytes::<Config>(bytes)?;
+            load_context.set_default_asset(LoadedAsset::new(config));
+            Ok(())
+        })
+    }
+
+    fn extensions(&self) -> &[&str] {
+        &["ron"]
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use bevy::log::LogPlugin;
 use bevy::prelude::*;
 
+mod config;
 mod cursor2d;
 mod hexagonal_coordinate;
 
+/// App state.
 #[derive(Clone, PartialEq, Eq, Debug, Hash)]
-enum GameState {
+enum AppState {
+    Setup,
     Title,
 }
 
@@ -16,11 +19,40 @@ fn main() {
             level: bevy::log::Level::DEBUG,
         }))
         .add_plugin(cursor2d::Cursor2dPlugin)
-        .add_state(GameState::Title)
+        .add_plugin(config::ConfigPlugin)
+        .add_state(AppState::Setup)
         .add_startup_system(spawn_camera)
+        .add_system_set(SystemSet::on_update(AppState::Setup).with_system(enter_title))
+        .add_system_set(SystemSet::on_exit(AppState::Setup).with_system(info_config))
         .run();
 }
 
+/// Spawn camera2d.
 fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+}
+
+/// Show config.
+fn info_config(
+    config_query: Query<&Handle<config::Config>>,
+    config_assets: ResMut<Assets<config::Config>>,
+) {
+    let config_handle = config_query.single();
+    if let Some(config) = config_assets.get(config_handle) {
+        info!("{:#?}", config);
+    } else {
+        info!("No config.");
+    }
+}
+
+/// Enter Title from Setup.
+fn enter_title(mut app_state: ResMut<State<AppState>>, mut count: Local<usize>) {
+    // Waiting to be able to get a config.
+    if *count >= 1 {
+        if let Ok(_) = app_state.set(AppState::Title) {
+        } else {
+            error!("failed to transition app state.");
+        }
+    }
+    *count += 1;
 }
