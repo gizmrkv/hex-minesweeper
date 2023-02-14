a221f948128cc37876f9b4c823718b465d4cf0b4
gizmrkv <gizmrkv@gmail.com>
2023-02-14 20:39:40 +0900

    update: LoadingConfig状態を追加

--- src/config.rs
+++ src/config.rs
@@ -6,6 +6,8 @@ use bevy::{
 };
 use serde::Deserialize;
 
+use crate::AppState;
+
 /// Plugin to allow access to app config.
 pub struct ConfigPlugin;
 
@@ -13,7 +15,11 @@ impl Plugin for ConfigPlugin {
     fn build(&self, app: &mut App) {
         app.add_asset::<Config>()
             .init_asset_loader::<ConfigLoader>()
-            .add_startup_system(load_config);
+            .add_startup_system(load_config)
+            .add_system_set(
+                SystemSet::on_update(AppState::LoadingConfig).with_system(enter_setup_state),
+            )
+            .add_system_set(SystemSet::on_exit(AppState::LoadingConfig).with_system(info_config));
     }
 }
 
@@ -21,6 +27,29 @@ impl Plugin for ConfigPlugin {
 fn load_config(asset_server: Res<AssetServer>, mut commands: Commands) {
     let handle: Handle<Config> = asset_server.load("config/config.ron");
     commands.spawn(handle);
+}
+
+/// Enter Title from Setup.
+fn enter_setup_state(mut app_state: ResMut<State<AppState>>, mut count: Local<usize>) {
+    // Waiting to be able to get a config.
+    if *count >= 1 {
+        if let Ok(_) = app_state.set(AppState::Setup) {
+            *count = 0;
+        } else {
+            error!("failed to transition app state.");
+        }
+    }
+    *count += 1;
+}
+
+/// Inform config.
+fn info_config(config_query: Query<&Handle<Config>>, config_assets: ResMut<Assets<Config>>) {
+    let config_handle = config_query.single();
+    if let Some(config) = config_assets.get(config_handle) {
+        info!("{:#?}", config);
+    } else {
+        info!("No config.");
+    }
 }
 
 /// app config.
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,12 @@ mod cursor2d;
 mod hexagonal_coordinate;
 mod hexagonal_cursor;
 mod hexagonal_table;
+mod title;
 
 /// App state.
 #[derive(Clone, PartialEq, Eq, Debug, Hash)]
 enum AppState {
+    LoadingConfig,
     Setup,
     Title,
 }
@@ -23,39 +25,12 @@ fn main() {
         .add_plugin(cursor2d::Cursor2dPlugin)
         .add_plugin(config::ConfigPlugin)
         .add_plugin(hexagonal_cursor::HexagonalCursorPlugin)
-        .add_state(AppState::Setup)
+        .add_state(AppState::LoadingConfig)
         .add_startup_system(spawn_camera)
-        .add_system_set(SystemSet::on_update(AppState::Setup).with_system(enter_title))
-        .add_system_set(SystemSet::on_exit(AppState::Setup).with_system(info_config))
         .run();
 }
 
 /// Spawn camera2d.
 fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
-}
-
-/// Show config.
-fn info_config(
-    config_query: Query<&Handle<config::Config>>,
-    config_assets: ResMut<Assets<config::Config>>,
-) {
-    let config_handle = config_query.single();
-    if let Some(config) = config_assets.get(config_handle) {
-        info!("{:#?}", config);
-    } else {
-        info!("No config.");
-    }
-}
-
-/// Enter Title from Setup.
-fn enter_title(mut app_state: ResMut<State<AppState>>, mut count: Local<usize>) {
-    // Waiting to be able to get a config.
-    if *count >= 1 {
-        if let Ok(_) = app_state.set(AppState::Title) {
-        } else {
-            error!("failed to transition app state.");
-        }
-    }
-    *count += 1;
 }
